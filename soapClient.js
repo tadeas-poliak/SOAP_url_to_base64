6d52cedf1b1792bec69dad649a225f8c36b26a58
tadeas-poliak <poliak.tada@gmail.com>
2024-10-16 22:08:32 +0200

    service modification

--- soapClient.js
+++ soapClient.js
@@ -2,6 +2,7 @@ import soap from 'soap'; // ES module import
 
 // const url = 'http://localhost:8001/SOAP.Demo.cls?wsdl';
 const url = 'https://soapurltobase64-production.up.railway.app/SOAP.demo.cls?wsdl';
+// const url = 'https://www.crcind.com/csp/samples/SOAP.Demo.CLS?WSDL=1'
 // Define an async function to handle the SOAP requests
 async function callSOAPService() {
     try {
@@ -12,9 +13,9 @@ async function callSOAPService() {
         // Define arguments for ProcessImage operation
         const imageArgs = { imageUrl: 'http://www.delbag.net/SAP/HENGST_logo_carton.png' };
 
-        // Call ProcessImage operation
-        const [imageResult] = await client.ProcessImageAsync(imageArgs);
-        console.log("ProcessImage Result: ", imageResult);
+        // // Call ProcessImage operation
+        // const [imageResult] = await client.ProcessImageAsync(imageArgs);
+        // console.log("ProcessImage Result: ", imageResult);
 
         // Call AddIntegers operation
         const addIntegersArgs = { intA: 5, intB: 10 };
